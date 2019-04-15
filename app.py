8498a6603f97fb46edadda36a9188ed356b550d6
LEO <asda2753596@gmail.com>
2019-04-15 14:46:05 +0800

    add update

--- app.py
+++ app.py
@@ -40,14 +40,14 @@ def handle_message(event):
     msg = event.message.text
     r = '很抱歉，我不懂你說甚麼'
 
-    if msg in [hi, Hi]:
+    if msg in ['hi', 'Hi']:
         r = '嗨'
     elif msg == '玉婕是笨蛋':
         r = '您真是聰明'
     elif '瑋杰' in msg:
         r = '你是想說瑋杰非常有天分嗎?'
-        
-    line_bot_api.reply_message(
+
+    line_bot_api.reply_message(s
         event.reply_token,
         TextSendMessage(text=r))
 
