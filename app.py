58d52ee91334a9d87db969a3d12a9b844013b302
LEO <asda2753596@gmail.com>
2019-04-15 14:37:03 +0800

    add update

--- app.py
+++ app.py
@@ -38,9 +38,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    r == '很抱歉，我不懂你說甚麼'
+    if msg in [hi, Hi]:
+        r = '嗨'
+    elif msg == '玉婕是笨蛋':
+        r = '您真是聰明'
+    elif '瑋杰' in msg:
+        r = '你是想說瑋杰非常有天分嗎?'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=msg))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
